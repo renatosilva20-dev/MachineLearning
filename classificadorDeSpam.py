9becd7c1f667020a88897509ce3139f4c7edb846
Renato Junior <rrenato.juniorso@gmail.com>
2025-12-10 10:18:58 -0300

    matriz da confusao adicionada ao classificador de spam

--- classificadorDeSpam.py
+++ classificadorDeSpam.py
@@ -1,5 +1,5 @@
 # %%
-import pandas as pd 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 # %%
 df = pd.read_csv('data/spam.csv', encoding='latin-1')
@@ -31,4 +31,18 @@ model.fit(X_train_vec, y_train)
 y_pred = model.predict(X_test_vec)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy of predict the Spam: {accuracy:.2f}')
+# %%
+# Visualizando algumas previsões
+# Matriz da confusão
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Not Spam', 'Spam'], yticklabels=['Not Spam', 'Spam'])
+plt.xlabel('Previsto')
+plt.ylabel('Atual')
+plt.title('Confusion Matrix')
+plt.show()
+
+# Observa-se que o modelo obteve uma alta acurácia na classificação de mensagens consideradas como SPAM
 # %%
