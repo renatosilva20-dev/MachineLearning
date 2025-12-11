2421354499229338b267a8b9978a3e5b17a00bdc
Renato <renatingod32@gmail.com>
2025-12-11 09:19:03 -0300

    Re-organizando a pasta para um treinamento utilizando modularização

--- semana_03/classificadorDeSpam.py
+++ semana_03/classificadorDeSpam.py
@@ -20,7 +20,7 @@ from sklearn.metrics import accuracy_score
 X = df['message']
 y = df['label']
 # %%
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 # %%
 vec = CountVectorizer()
 X_train_vec = vec.fit_transform(X_train)
